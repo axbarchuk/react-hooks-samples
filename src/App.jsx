a80dd2751cf142c506b7168c8aebc7ab23a2c904
Alex Barchuk <ax.barchuk@gmail.com>
2020-03-05 18:29:00 +0200

    Sample of using reducer hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useReducer} from 'react'
 import './App.css'
 
 const App = () => {
@@ -7,9 +7,42 @@ const App = () => {
             <header className="App-header">
                 <a href="https://bcode.dev">Bcode.dev</a>
                 <p><b>Hooks API</b> in samples.</p>
+
+                <MyCounter />
             </header>
         </div>
     )
 }
+
+const initialState = {count: 0}
+const Action = {
+    INCREMENT: 'increment',
+    DECREMENT: 'decrement',
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case Action.INCREMENT:
+            return {count: state.count + 1}
+        case Action.DECREMENT:
+            return {count: state.count - 1}
+        default:
+            throw new Error()
+    }
+}
+
+const MyCounter = () => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+    return (
+        <>
+            <p>Count: {state.count}</p>
+            <div>
+                <button onClick={() => dispatch({type: Action.DECREMENT})}>-</button>
+                <button onClick={() => dispatch({type: Action.INCREMENT})}>+</button>
+            </div>
+        </>
+    )
+}
+
 
 export default App
