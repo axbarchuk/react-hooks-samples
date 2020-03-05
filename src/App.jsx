ef2c11c5797619433b0887f8799b094c2fdffe3a
Alex Barchuk <ax.barchuk@gmail.com>
2020-03-05 18:03:31 +0200

    Sample of using context hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './App.css'
+
+const colors = {
+    blue: "#0d90e2",
+    red: "#ea172e",
+}
+
+const ColorContext = React.createContext(colors.blue)
 
 const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <a href="https://bcode.dev">Bcode.dev</a>
-                <p><b>Hooks API</b> in samples.</p>
-            </header>
-        </div>
+        <ColorContext.Provider value={colors}>
+            <div className="App">
+                <header className="App-header">
+                    <a href="https://bcode.dev">Bcode.dev</a>
+                    <p style={ {color:colors.blue} }><b>Hooks API</b> in samples.</p>
+
+                    <MyComponent />
+                </header>
+            </div>
+        </ColorContext.Provider>
     )
+}
+
+// Independent component
+const MyComponent = () => {
+    const colors = useContext(ColorContext);
+    return <div style={{ color: colors.red }}>My independent Component</div>;
 }
 
 export default App
