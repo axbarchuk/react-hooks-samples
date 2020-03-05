dbbafb47c06d021bdf8b1cad6be0f3fe761d8392
Alex Barchuk <ax.barchuk@gmail.com>
2020-03-05 16:48:01 +0200

    Cleaned up useless code.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="https://bcode.dev">Bcode.dev</a>
+        <p><b>Hooks API</b> in samples.</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
