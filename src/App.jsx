9af591a8b80b0b23ad998a12b0629a51c6021996
Alex Barchuk <ax.barchuk@gmail.com>
2020-03-05 17:30:30 +0200

    Sample of using effect hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a href="https://bcode.dev">Bcode.dev</a>
-        <p><b>Hooks API</b> in samples.</p>
-      </header>
-    </div>
-  )
+    const [value, setValue] = useState("")
+    const [result, setResult] = useState("Wrong")
+
+    useEffect(
+        () => setResult(value === "hello" ? "Correct" : "Wrong"),
+        [value]
+    )
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <a href="https://bcode.dev">Bcode.dev</a>
+                <p><b>Hooks API</b> in samples.</p>
+
+                <span>Enter "hello" to see the result.</span>
+                <input
+                    value={value}
+                    onChange={(event) => setValue(event.target.value.toLowerCase())}
+                />
+                <p>Result: <b>{result}</b></p>
+            </header>
+        </div>
+    )
 }
 
 export default App
