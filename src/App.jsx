26f84cb039364776b1a3e49818597e0f0a47e884
Alex Barchuk <ax.barchuk@gmail.com>
2020-03-05 17:31:51 +0200

    Sample of using state hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a href="https://bcode.dev">Bcode.dev</a>
-        <p><b>Hooks API</b> in samples.</p>
-      </header>
-    </div>
-  )
+    const [count, setCount] = useState(0)
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <a href="https://bcode.dev">Bcode.dev</a>
+                <p><b>Hooks API</b> in samples.</p>
+
+                <h1>{ count }</h1>
+
+                <button onClick={() => setCount(count + 1)}>Click here</button>
+            </header>
+        </div>
+    )
 }
 
 export default App
