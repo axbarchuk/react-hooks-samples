7d09719c566ef355bbb64d294ff790df6a2f8001
Alex Barchuk <ax.barchuk@gmail.com>
2020-03-17 16:43:38 +0200

    Sample of using ref hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './App.css'
+import image1 from './images/emptyLike.png'
+import image2 from './images/like.png'
 
 const App = () => {
     return (
         <div className="App">
-            <header className="App-header">
                 <a href="https://bcode.dev">Bcode.dev</a>
                 <p><b>Hooks API</b> in samples.</p>
-            </header>
+
+                <LikeComponent />
         </div>
     )
+}
+
+const LikeComponent = () => {
+    const imageRef = useRef(null)
+    return <img
+        src={image1}
+        ref={imageRef}
+        width={50}
+        onClick={() => imageRef.current.src = image2}
+    />
 }
 
 export default App
